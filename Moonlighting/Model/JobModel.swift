0bb722aae042617da1188ace636260681f5e75c8
SonataGirl <sonatagirl@>
2023-10-30 21:38:09 +0300

    create project and models

---
+++ Moonlighting/Model/JobModel.swift
@@ -0,0 +1,16 @@
+//
+//  Job.swift
+//  Moonlighting
+//
+//  Created by Sonata Girl on 30.10.2023.
+//
+
+import Foundation
+
+struct JobModel {
+    let salary: Double
+    let profession, id: String
+    let date: Date
+    let employer: String
+    let logo: URL?
+}
---
+++ Moonlighting/View/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moonlighting
+//
+//  Created by Sonata Girl on 30.10.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
